528d87bfe20c355d4e2488ce38bf4805f1507485
Subikesh <subikeshps@gmail.com>
2020-11-23 10:38:37 +0530

    Created models and migrations

--- exam_registration/settings.py
+++ exam_registration/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'exam_registration.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Exam_regn',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
--- main/models.py
+++ main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.db.models.fields import DateField, EmailField, FloatField
+from django.db.models.fields.related import ForeignKey, ManyToManyField, OneToOneField
 
-# Create your models here.
+# Contains details of students
+class Student(models.Model):
+    GENDER_CHOICE = [('Male', 'Male'), ('Female', 'Female')]
+
+    St_id = models.CharField(max_length=20, primary_key=True)
+    St_name = models.CharField(max_length=100)
+    DOB = models.DateField()
+    Gender = models.CharField(max_length=7, choices=GENDER_CHOICE, null=True)
+    Qualification = models.CharField(max_length=30)
+    Semester = models.IntegerField()
+    Email = models.EmailField()
+
+# Stores objects for number of attempts done on each subjects
+class Subject_attempts(models.Model):
+    Student = ForeignKey(Student, on_delete=models.CASCADE)
+    Sub_code = models.CharField(max_length=10)
+    attempts = models.IntegerField(default=0)
+
+class Register(models.Model):
+    Student = ForeignKey(Student, on_delete=models.CASCADE)
+    RegDate = DateField(auto_now_add=True)
+    TotalFee = FloatField()
+
+class Subject(models.Model):
+    Name = models.CharField(max_length=70)
+    Registration = ManyToManyField(Register)
+    Duration = models.IntegerField()
+    DateofExam = models.DateField()
+    Fee = models.FloatField()
