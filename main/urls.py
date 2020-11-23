38be1d9db6d2c55c3600de0b487f6427c7dc905d
Subikesh <subikeshps@gmail.com>
2020-11-23 12:54:27 +0530

    User Login

--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,4 @@
+from main.views import log_in
 from . import views
 from django.urls import path
 
@@ -5,5 +6,7 @@ app_name = 'main'
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    path("login/", views.log_in, name="log_in"),
+    path("logout/", views.log_out, name="log_out"),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from main.models import Student, Subject, Subject_attempts, Register
 
 def homepage(request):
     context = {"home":"active"}
     return render(request, "home.html", context)
+
+# Login for the username and password
+def log_in(request):
+    context = {'log_in':'active'}
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = auth.authenticate(username = username, password = password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "You are successfully logged in.")
+            return redirect('/')
+        else:
+            messages.error(request, "Username or Password is incorrect.")
+    return render(request, 'login.html', context)
+
+# Logout the current user
+def log_out(request):
+    auth.logout(request)
+    messages.info(request, "You have been logged out of the website.")
+    return redirect('/')
